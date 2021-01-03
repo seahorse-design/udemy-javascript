5cddb710a7c805bee94e4ab3db94bc143ef0f392
saki matsushita <saki.seahorse@gmail.com>
2021-01-04 00:33:25 +0900

    セクション6まで

---
+++ 04/09/main.js
@@ -0,0 +1,9 @@
+// const a = 'hello';
+// a = 'bye'; //エラー
+
+const b = {
+    prop: 'hello'
+}
+//b = {} //エラー
+b.prop = 'bye';
+console.log(b);
---
+++ 04/11/main.js
@@ -0,0 +1,8 @@
+const a = {
+    prop: 0
+}
+const b = {
+    prop: 0
+}
+console.log(a === b); //falseになる
+console.log(a.prop === b.prop); //中身（prop）を調べたい場合に記述する
